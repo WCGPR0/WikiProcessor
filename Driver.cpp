c9cf8628140677228d9b6dc5cd362ba76b8fd667
Alex Rivie <4mless@gmail.com>
2014-04-22 22:28:55 -0400

    added timer

--- Driver.cpp
+++ Driver.cpp
@@ -3,23 +3,31 @@
 #include <iostream>
 #include <fstream>
 #include "Processor.h"
+#include <sys/time.h>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-const string stopFile = "stop.txt";
-string fileName;
-if (argc >= 2) {
-if (argc > 2) std::cerr << "Invalid use.\nUsage: ./wikiParse input_file_name" << endl;
-fileName = argv[1];
-}
-else {
-cout << "Please input the title of the file to processes" << endl;
-string fileName;
-cin >> fileName;
-}
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+	const string stopFile = "stop.txt";
+	string fileName;
+	if (argc >= 2) {
+	if (argc > 2) std::cerr << "Invalid use.\nUsage: ./wikiParse input_file_name" << endl;
+	fileName = argv[1];
+	}
+	else {
+	cout << "Please input the title of the file to processes" << endl;
+	string fileName;
+	cin >> fileName;
+	}
 
-Processor* myProcessor = new Processor(fileName);
+	Processor* myProcessor = new Processor(fileName);
 
-delete myProcessor;
-return 0;
+	delete myProcessor;
+
+	gettimeofday(&end, NULL);
+	float runtime = (end.tv_usec - start.tv_usec);
+	cout << runtime << endl;
+	
+	return 0;
 }
